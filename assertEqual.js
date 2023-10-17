c704e2cae60f73a7a72ecf58ecd1828f9e7d9a87
Ana <anmeuri@gmail.com>
2023-10-16 21:54:41 -0700

    Simplified use of if else in the code and added more test cases

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   let positiveMessage = `✔✔ Assertion Passed: ${actual} === ${expected} ✔✔`;
   let negativeMessage = `🛑 Assertion Failed: ${actual} !== ${expected} 🛑`;
 
   if (actual === expected) {
     console.log(positiveMessage);
-  } else if (actual !== expected) {
+  } else {
     console.log(negativeMessage);
   }
 };
@@ -13,3 +13,5 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("Ana", "Ana");
+assertEqual(53, 35);
