927f9ed2f9fdf6a367045fc62d91626353434acd
Ana <anmeuri@gmail.com>
2023-10-18 13:56:28 -0700

    Fixed first conditional - it didn't have a return before - to look more readable

--- middle.js
+++ middle.js
@@ -26,6 +26,7 @@ const eqArrays = function(arr1, arr2) {
 const middle = function(arr) {
   let middleArr = [];
   if (arr.length < 3) {
+    return middleArr;
   } else if (arr.length % 2 !== 0) {
     middleArr.push(arr[Math.floor(arr.length / 2)]);
   } else if (arr.length % 2 === 0) {
