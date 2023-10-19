8146b29a30a0dd061e9e059bf34f69a500213f04
Ana <anmeuri@gmail.com>
2023-10-18 21:28:40 -0700

    Created letterPositions.js function that will return aan object with string letters as properties and arrays with its indexes as values

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const assertArraysEqual = function(arr1, arr2) {
+  let positiveMessage = `🤩 Assertion Passed! The arrays are equal! 🤩`;
+  let negativeMessage = `😨 Assertion Failed! The arrays are different... 😨`;
+
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(positiveMessage);
+  } else {
+    console.log(negativeMessage);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      // If letter is already in the results, push index to the array:
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+      // If letter is not in the results yet, create array with its index:
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
