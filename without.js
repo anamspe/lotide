d3dbb820459e187563b462dd0def6ccaa2370b4e
Ana <anmeuri@gmail.com>
2023-10-18 21:34:35 -0700

    Updated all files with function assertArraysEqual

--- without.js
+++ without.js
@@ -2,7 +2,7 @@ const assertArraysEqual = function(arr1, arr2) {
   let positiveMessage = `🤩 Assertion Passed! The arrays are equal! 🤩`;
   let negativeMessage = `😨 Assertion Failed! The arrays are different... 😨`;
 
-  if (eqArrays(arr1, arr2) === true) {
+  if (eqArrays(arr1, arr2)) {
     console.log(positiveMessage);
   } else {
     console.log(negativeMessage);
