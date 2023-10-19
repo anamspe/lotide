d18817f56d661dd0fa22e7e6f17e58b05ac0ac41
Ana <anmeuri@gmail.com>
2023-10-18 23:05:28 -0700

    Created findKeyByValue.js function that searches for a key in an object based on a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  let positiveMessage = `✔✔ Assertion Passed: ${actual} === ${expected} ✔✔`;
+  let negativeMessage = `🛑 Assertion Failed: ${actual} !== ${expected} 🛑`;
+
+  if (actual === expected) {
+    console.log(positiveMessage);
+  } else {
+    console.log(negativeMessage);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let keys = Object.keys(object);
+  for (let key of keys) {
+    if (value === object[key]){
+      return key;
+    } 
+  }
+};
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  musical: "Zoey's Extraordinary Playlist"
+};
+
+// findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Zoey's Extraordinary Playlist"), "musical")
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Killing Eve"), undefined)
