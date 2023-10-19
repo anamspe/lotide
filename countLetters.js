d23f49437ed8afe59e82f10fa2437242f1b97458
Ana <anmeuri@gmail.com>
2023-10-18 19:52:07 -0700

    Fixed function to return instead of console.log, simplified conditionals and added test cases

--- countLetters.js
+++ countLetters.js
@@ -13,14 +13,19 @@ const countLetters = function(string) {
   let count = {}
 
   for (letter of string) {
-    if (letter !== ' ' && count[letter]) {
+    if (letter !== ' ') {
+      if (count[letter]) {
       count[letter] += 1; 
-    } else if (letter !== ' ' && !count[letter]) {
+    } else {
       count[letter] = 1;
     }
+   }
   }
-
-  console.log(count);
+  return count;
 };
 
-countLetters("the streets are made of square blocks");
+let result = countLetters("hello");
+assertEqual(result["h"], 1);
+assertEqual(result["e"], 1);
+assertEqual(result["l"], 2);
+assertEqual(result["o"], 1);
