f305077d919bb1926d7de694db6b4b71d847b36d
Ana <anmeuri@gmail.com>
2023-10-19 22:36:01 -0700

    update

--- takeUntil.js
+++ takeUntil.js
@@ -35,15 +35,6 @@ const takeUntil = function(array, callback) {
   return newArr;  
 };
 
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
-
-// console.log('---');
-
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// console.log(results2);
 
 console.log(takeUntil([24, 26, 27, 29, 32], x => x > 28))
 
