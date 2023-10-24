102165972de8a19072fb9db00860eae59c97c6ff
Ana <anmeuri@gmail.com>
2023-10-23 17:38:15 -0700

    Refactored tests for headTest.js so it uses mocha for testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([90]), 90);
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1 );
+  });
+  
+  it("returns '90' for ['90']", () => {
+    assert.strictEqual(head([90]), 90);
+  });
+  
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
