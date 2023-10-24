bfce4aead7cf7fb658341cf4a4678dfbc5a2eecb
Ana <anmeuri@gmail.com>
2023-10-23 18:18:18 -0700

    Incorporated Mocha and Chai to tailTest.js and middleTest.js

--- middle.js
+++ middle.js
@@ -1,7 +1,7 @@
 const middle = function(arr) {
   let middleArr = [];
   if (arr.length < 3) {
-    return "No middle value";
+    return [];
   } else if (arr.length % 2 !== 0) {
     middleArr.push(arr[Math.floor(arr.length / 2)]);
   } else if (arr.length % 2 === 0) {
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,29 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it('returns an empty array ([]) for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-assertArraysEqual(middle([1]), 'No middle value');
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+  it('returns an empty array ([]) for [2, 3]', () => {
+    assert.deepEqual(middle([2, 3]), []);
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns ['little'] for ['home', 'little', 'sweet']", () => {
+    assert.deepEqual(middle(['home', 'little', 'sweet']), ['little']);
+  });
+
+});
+
+// assertArraysEqual(middle([1]), 'No middle value');
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
